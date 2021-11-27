7c3f2d79486797c693d3fed6916ea8e44769e437
Grunet <sadikshahidain@gmail.com>
2021-11-27 14:14:38 -0600

    fix(depsadapter.ts): swallowing errors without a code

--- src/shared/depsAdapter.ts
+++ src/shared/depsAdapter.ts
@@ -11,10 +11,11 @@ try {
   //.default.default is used in the ESM build when the "import" call wraps the underlying "require" call output with its own default export wrapping
   userEventInstance = result.default.default ?? result.default;
 } catch (e) {
-  if (!e.code?.includes("MODULE_NOT_FOUND")) {
+  if (e.code && e.code.includes("MODULE_NOT_FOUND")) {
     throw e;
   }
 
+  console.error(e);
   console.warn(
     "Unable to find @testing-library/user-event. Proceeding without it."
   ); //TODO - consider making these customizable via the log-level or a custom logger
@@ -26,10 +27,11 @@ try {
     prettyDOM: prettyDOMInstance,
   } = await import("@testing-library/dom"));
 } catch (e) {
-  if (!e.code?.includes("MODULE_NOT_FOUND")) {
+  if (e.code && e.code.includes("MODULE_NOT_FOUND")) {
     throw e;
   }
 
+  console.error(e);
   console.warn("Unable to find @testing-library/dom. Proceeding without it.");
 }
 
